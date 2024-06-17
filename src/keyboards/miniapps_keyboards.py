0ce2e2d1435f72a9abb1f8eacbddfdba73bdcecb
Kirill <kdberdyshev@gmail.com>
2024-06-17 12:20:25 +0300

    Add a bunch of stuff

--- src/keyboards/miniapps_keyboards.py
+++
@@ -1,7 +0,0 @@
-from aiogram import types
-def addEmployeeKeyboard():
-    my_web_app = types.web_app_info.WebAppInfo(url="https://9c76-195-19-48-107.ngrok-free.app")
-    kb = [[types.KeyboardButton(text='Добавить сотрудника', web_app=my_web_app)]]
-    keyboard = types.ReplyKeyboardMarkup(row_width=1, keyboard=kb, resize_keyboard=True)
-    return keyboard
-
